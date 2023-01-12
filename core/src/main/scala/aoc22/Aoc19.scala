09ff0f11dcf15aa2794815595c5a229648c54cfb
Thomas Dufour <chwthewke@gmail.com>
2023-01-12 23:07:39 +0100

    mmmh

--- core/src/main/scala/aoc22/Aoc19.scala
+++ core/src/main/scala/aoc22/Aoc19.scala
@@ -1,14 +1,14 @@
 package aoc22
 
-import cats.Monoid
 import cats.PartialOrder
-import cats.derived.semiauto
+import cats.data.NonEmptyVector
 import cats.effect.Sync
 import cats.kernel.Group
 import cats.parse.Parser
 import cats.syntax.all._
 import enumeratum.Enum
 import enumeratum.EnumEntry
+import scala.annotation.tailrec
 
 class Aoc19[F[_]: Sync]( srcFile: String, live: Boolean ) extends Day.Of[F]( srcFile, live ) {
   import Aoc19._
@@ -18,32 +18,45 @@ class Aoc19[F[_]: Sync]( srcFile: String, live: Boolean ) extends Day.Of[F]( src
       .through( parseLines( parsers.blueprint ) )
       .compile
       .toVector
-      .map( bs => s"parsed ${bs.size} blueprints" )
+      .flatMap(
+        bs =>
+          Sync[F].delay(
+            bs.map( b => s"BP #${b.id}: ${naiveOpt( b )}" ).mkString( "\n" )
+          )
+      )
+
   override def bonus: F[String] = ???
 }
 
 object Aoc19 {
 
-  case class Counts( ore: Int = 0, clay: Int = 0, obsidian: Int = 0, geode: Int = 0 ) {}
+  case class Counts( ore: Int = 0, clay: Int = 0, obsidian: Int = 0, geode: Int = 0 )
 
   object Counts {
-    @inline
-    private def compare( nextComparison: Int, rest: Int => Double )( expectedComparison: Int ): Double =
-      if (expectedComparison * nextComparison == -1) Double.NaN
-      else rest( if (expectedComparison == 0) nextComparison else expectedComparison )
 
-    implicit val countsPartialOrder: PartialOrder[Counts] = PartialOrder.from { ( c1, c2 ) =>
-      compare(
-        c1.clay.compareTo( c2.clay ),
-        compare(
-          c1.obsidian.compareTo( c2.obsidian ),
-          compare(
-            c1.geode.compareTo( c2.geode ),
-            _.toDouble
-          )
-        )
-      )( c1.ore.compareTo( c2.ore ) )
+    private val fields: List[Counts => Int] =
+      List( _.ore, _.clay, _.obsidian, _.geode )
+
+    def partialCompare( left: Counts, right: Counts ) = {
+
+      @tailrec
+      def partialCompareLoop(
+          expected: Int,
+          fields: List[Counts => Int]
+      ): Double =
+        fields match {
+          case Nil => expected.toDouble
+          case field :: more =>
+            val fieldCmp = field( left ).compareTo( field( right ) )
+            if (expected == 0 || fieldCmp == expected) partialCompareLoop( fieldCmp, more )
+            else if (fieldCmp == 0) partialCompareLoop( expected, more )
+            else Double.NaN
+        }
+
+      partialCompareLoop( 0, fields )
     }
+
+    implicit val countsPartialOrder: PartialOrder[Counts] = PartialOrder.from( partialCompare )
 
     val empty: Counts = Counts()
 
@@ -61,30 +74,85 @@ object Aoc19 {
       resources: Counts,
       robots: Counts
   ) {
-    def next( blueprint: Blueprint, orderOpt: Option[Item] ): Option[Inventory] = orderOpt match {
-      case Some( order ) =>
-        val orderCost = blueprint.costOf( order )
+    def moves( blueprint: Blueprint ): NonEmptyVector[( Option[Item], Counts )] =
+      NonEmptyVector(
+        ( None, Counts.empty ),
+        Item.values.mapFilter { item =>
+          val cost = blueprint.costOf( item )
+          Option.when( cost <= resources )( ( Some( item ), cost ) )
+        }
+      )
 
-        Option.when( orderCost <= resources )(
-          Inventory(
-            resources.remove( orderCost ) |+| robots,
-            robots |+| order.counts
-          )
-        )
-      case None =>
-        Inventory(
-          resources |+| robots,
-          robots
-        ).some
-    }
+    def next( orderOpt: Option[Item], cost: Counts ): Inventory =
+      Inventory(
+        resources.remove( cost ) |+| robots,
+        orderOpt.foldLeft( robots )( ( r, i ) => r |+| i.counts )
+      )
 
+    def nextMoves( blueprint: Blueprint ): NonEmptyVector[Inventory] =
+      moves( blueprint ).map( (next _).tupled )
   }
 
   object Inventory {
-    val init: Inventory = Inventory(
-      Counts.empty,
-      Counts( ore = 1 )
-    )
+    val init: Inventory = Inventory( Counts.empty, Counts( ore = 1 ) )
+
+    def partialCompare( left: Inventory, right: Inventory ): Double = {
+      val resourcesCmp = Counts.partialCompare( left.resources, right.resources )
+      if (resourcesCmp.isNaN) Double.NaN
+      else {
+        val robotsCmp = Counts.partialCompare( left.robots, right.robots )
+        if (robotsCmp == 0 || resourcesCmp == robotsCmp) resourcesCmp
+        else if (resourcesCmp == 0) robotsCmp
+        else Double.NaN
+      }
+    }
+
+    implicit val inventoryPartialOrder: PartialOrder[Inventory] = PartialOrder.from( partialCompare )
+  }
+
+  case class Inventories( inventories: Vector[Inventory] ) extends AnyVal {
+    def length: Int = inventories.size
+
+    def add( candidate: Inventory ): Inventories = {
+      @tailrec
+      def addLoop( ix: Int ): Vector[Inventory] =
+        if (ix >= inventories.length) inventories :+ candidate
+        else {
+          val cmp = Inventory.partialCompare( candidate, inventories( ix ) )
+          if (cmp <= 0) inventories
+          else if (cmp > 0)
+            inventories.take( ix ) ++ inventories.drop( ix + 1 ).filterNot( _ <= candidate ) :+ candidate
+          else
+            addLoop( ix + 1 )
+        }
+
+      Inventories( addLoop( 0 ) )
+    }
+  }
+
+  object Inventories {
+    val empty: Inventories = Inventories( Vector.empty )
+    val init: Inventories  = Inventories( Vector( Inventory.init ) )
+  }
+
+  val MaxTurn: Int = 24
+
+  def naiveOpt( blueprint: Blueprint ): Int = {
+    @tailrec
+    def loop( turn: Int, states: Inventories ): Int = {
+      println( s"Turn ${turn + 1} from ${states.length} states" )
+      if (turn == MaxTurn - 1) // nothing we do on the last turn changes anything
+        states.inventories.map( _.next( None, Counts.empty ) ).map( _.resources.geode ).max
+      else
+        loop(
+          turn + 1,
+          states.inventories.foldLeft( Inventories.empty )(
+            ( invs, state ) => state.nextMoves( blueprint ).foldLeft( invs )( _.add( _ ) )
+          )
+        )
+    }
+
+    loop( 0, Inventories.init )
   }
 
   sealed abstract class Item( val counts: Counts ) extends EnumEntry
@@ -107,10 +175,10 @@ object Aoc19 {
       obsidianPerGeodeRobot: Int
   ) {
     def costOf( item: Item ): Counts = item match {
-      case Item.Ore      => Counts( orePerOreRobot, 0, 0, 0 )
-      case Item.Clay     => Counts( orePerClayRobot, 0, 0, 0 )
-      case Item.Obsidian => Counts( orePerObsidianRobot, clayPerObsidianRobot, 0, 0 )
-      case Item.Geode    => Counts( orePerGeodeRobot, 0, obsidianPerGeodeRobot, 0 )
+      case Item.Ore      => Counts( ore = orePerOreRobot )
+      case Item.Clay     => Counts( ore = orePerClayRobot )
+      case Item.Obsidian => Counts( ore = orePerObsidianRobot, clay = clayPerObsidianRobot )
+      case Item.Geode    => Counts( ore = orePerGeodeRobot, obsidian = obsidianPerGeodeRobot )
     }
   }
 
